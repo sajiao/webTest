33b27461a7b710c9975e404dd56b7f69885fc5d8
eleven <yijun-eleven@163.com>
2015-10-11 23:20:50 +0800

    add test

---
+++ Common/Common.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Common
+{
+    public static class Utility
+    {
+
+        public static int GetWorkDay(DateTime dtStart, DateTime dtEnd)
+        {
+            TimeSpan ts = dtEnd.AddDays(1).Subtract(dtStart);
+            int countday = ts.Days;
+            int workday = 0;
+
+            for (int i = 0; i < countday; i++)
+            {
+                DateTime tempdt = dtStart.Date.AddDays(i);
+                if (tempdt.DayOfWeek != System.DayOfWeek.Saturday && tempdt.DayOfWeek != System.DayOfWeek.Sunday)
+                {
+                    workday++;
+                }
+            }
+            return workday;
+        }
+
+    }
+}
